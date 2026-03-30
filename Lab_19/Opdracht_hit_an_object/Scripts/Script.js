d44114cc3120fb71561f939b3d360f7ba9a464c9
r1103868 <r1103868@student.vives.be>
2026-03-30 18:07:18 +0200

    Update Script.js

--- Lab_19/Opdracht_hit_an_object/Scripts/Script.js
+++ Lab_19/Opdracht_hit_an_object/Scripts/Script.js
@@ -24,7 +24,7 @@
         let target = document.getElementById("target");
         let scores = document.getElementsByClassName("score");
         let welke =  Math.round(Math.random() * 4)
-        target.src = "images/" + welke + ".png";
+        target.src = "./images/" + welke + ".png";
 
     }
     const update = () => {
